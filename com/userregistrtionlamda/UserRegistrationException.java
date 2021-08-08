21ff276f27d55e1385cb9eb69f6a28b965e92ec6
manjunathhalli <manjunathhalli629@gmail.com>
2021-08-08 23:43:24 +0530

    using lamda finction

---
+++ com/userregistrtionlamda/UserRegistrationException.java
@@ -0,0 +1,9 @@
+package com.userregistrtionlamda;
+
+public class UserRegistrationException extends Exception {
+
+    public UserRegistrationException(String message){
+        super(message);
+    }
+
+}
